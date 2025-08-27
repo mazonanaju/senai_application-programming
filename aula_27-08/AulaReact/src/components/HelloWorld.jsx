e5a265c4e7683135c888a0b70068f4bcc9ba4378
mazonanaju <anajuliam.dev@gmail.com>
2025-08-27 09:51:37 -0300

    creating components

---
+++ aula_27-08/AulaReact/src/components/HelloWorld.jsx
@@ -0,0 +1,5 @@
+export const HelloWorld = () => {
+    return(
+        <h1>Hello, World! (in react)</h1>
+    )
+}
---
+++ aula_27-08/AulaReact/src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode > {/* não faz diferença, só permite ver alguns bugs */}
+    <App />
+  </StrictMode>,
+)
